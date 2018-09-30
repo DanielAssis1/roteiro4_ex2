f5b17d0e896142d71a14a704ff1221bd03a58b93
DanielAssis1 <43318923+DanielAssis1@users.noreply.github.com>
2018-09-30 12:27:50 -0300

    alt

--- roteiro4_ex2/Program.cs
+++ roteiro4_ex2/Program.cs
@@ -20,32 +20,100 @@ namespace roteiro4_ex2
 • Porcentagem total (homens e mulheres) com experiência no serviço
 Faça um programa que calcule as informações para um total de 30 candidatos */
 
-            int i, quntM=0, quntH=0, mediaH=0, mediaMExp=0, exp=0, Mexp=0, H35a45=0, menoriM=0;
-            char sexo=' ';
+            int i, qntM=0, qntH=0, somaiH=0 , somaiM=0, exp=0, Mexp=0, H35a45=0, menoriM=0, idade;
+            char sexo = ' ', experiencia = ' ';
             bool check=false;
             for (i = 0; i < 30; i++)
             {
 
                 do
                 {
-                    Console.Write("Digite o sexo do candidato [M] para Masculino ou [F] para Feminino: ");
+                    Console.Write("\nDigite o sexo do candidato [M] para Masculino ou [F] para Feminino: ");
                     sexo = char.Parse(Console.ReadLine().ToUpper());
 
                     if(!(sexo.Equals('M') || sexo.Equals('F')))
                     {
-                        Console.WriteLine("O valor digitado é inválido digite novamente");
+                        Console.WriteLine("\nO valor digitado é inválido digite novamente");
                         check = true;
                     }
                     else
                     {
+                        if (sexo.Equals('M'))
+                        {
+                            qntH++;
+                        }
+                        else if (sexo.Equals('F'))
+                        {
+                            qntM++;
+                        }
 
+                        Console.WriteLine("O candidato possui experiencia? [S] para Sim ou [N] para Não");
+                        experiencia = char.Parse(Console.ReadLine().ToUpper());
+
+                        if (!(experiencia.Equals('S') || experiencia.Equals('N')))
+                        {
+                            Console.WriteLine("\nO valor digitado é inválido digite novamente");
+                            check = true;
+                        }
+                        else
+                        {
+                            if (experiencia.Equals('S'))
+                            {
+                                exp++;
+                            }
+                            if(experiencia.Equals('S') && sexo.Equals('F'))
+                            {
+                                Mexp++;
+                            }
+                            Console.Write("Digite a idade do candidato: ");
+                            idade = int.Parse(Console.ReadLine());
+
+                            if (idade <= 0)
+                            {
+                                Console.WriteLine("\nO valor digitado é inválido digite novamente");
+                                check = true;
+                            }
+                            else
+                            {
+                                if (sexo.Equals('M'))
+                                {
+                                    somaiH += idade;
+                                    if (idade >= 35 && idade <= 45)
+                                    {
+                                        H35a45++;
+                                    }
+                                }
+                                else if (sexo.Equals('F'))
+                                {
+                                    somaiM += idade;
+
+                                    if (i == 0) {
+                                        menoriM = idade;
+                                    }
+                                    else
+                                    {
+                                        if (menoriM < idade)
+                                        {
+                                            menoriM = idade;
+                                        }
+                                    }
+                                }
+                            }
+                            
+                        }
+
+
+                        
+                        
                     }
 
                 } while (check==true);
 
+               
+
                 i++;
             }
-            
+            Console.WriteLine("");
 
 
 
