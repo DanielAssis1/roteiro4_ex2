a79e10fd4f1191743dd496b624fcb89f90ab6e63
DanielAssis1 <43318923+DanielAssis1@users.noreply.github.com>
2018-09-30 11:36:07 -0300

    ss

--- roteiro4_ex2/Program.cs
+++ roteiro4_ex2/Program.cs
@@ -10,6 +10,46 @@ namespace roteiro4_ex2
     {
         static void Main(string[] args)
         {
+            /*Exercício 2 –
+• Quantidade de candidatos do sexo feminino
+• Quantidade de candidatos do sexo masculino
+• Idade média dos homens
+• Idade média das mulheres com experiência
+• Porcentagem dos homens entre 35 e 45 anos entre o total dos homens
+• Menor idade entre as mulheres
+• Porcentagem total (homens e mulheres) com experiência no serviço
+Faça um programa que calcule as informações para um total de 30 candidatos */
+
+            int i, quntM=0, quntH=0, mediaH=0, mediaMExp=0, exp=0, Mexp=0, H35a45=0, menoriM=0;
+            char sexo=' ';
+            bool check=false;
+            for (i = 0; i < 30; i++)
+            {
+
+                do
+                {
+                    Console.Write("Digite o sexo do candidato [M] para Masculino ou [F] para Feminino: ");
+                    sexo = char.Parse(Console.ReadLine().ToUpper());
+
+                    if(!(sexo.Equals('M') || sexo.Equals('F')))
+                    {
+                        Console.WriteLine("O valor digitado é inválido digite novamente");
+                        check = true;
+                    }
+                    else
+                    {
+
+                    }
+
+                } while (check==true);
+
+                i++;
+            }
+            
+
+
+
+
         }
     }
 }
